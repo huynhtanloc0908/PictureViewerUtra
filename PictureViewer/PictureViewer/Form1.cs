bb3f113a0d3160ccc7a2cee6dabd75afbd559641
huynhtanloc0908 <tanlochuynh112@gmail.com>
2024-09-18 12:12:25 +0700

    PictureViewerUtra

---
+++ PictureViewer/PictureViewer/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            o.Title = "File Explorer";
+            o.Filter = "File anh(*.jpg , *.png , *.gif) |*.jpg;*.png;*.gif; |All File|*.*";
+            if(o.ShowDialog() == DialogResult.OK )
+            {
+                pictureBox1.Image= Image.FromFile( o.FileName );
+            }
+        }
+    }
+}
